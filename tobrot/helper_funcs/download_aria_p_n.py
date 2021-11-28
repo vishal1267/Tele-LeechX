ffd1497ef949c2088b07557192c9b77f68db50f6
vishal1267 <95161786+vishal1267@users.noreply.github.com>
2021-11-28 15:06:28 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -302,7 +302,7 @@ async def call_apropriate_function(
                         f"<a href='tg://user?id={user_id}'><i>🗃 Your Uploaded Files !!</i></a>\n\n"
                     )
                     message_to_send = mention_req_user + message_to_send
-                    message_to_send = message_to_send + "\n\n" + "#Uploads\n\n<b>💥 <i>Powered By : @JNS_BOTS ❤️</i> </b>"
+                    message_to_send = message_to_send + "\n\n" + "#Uploads\n\n<b>💥 <i>Powered By : @imTheVishal ❤️</i> </b>"
                 else:
                     message_to_send = "<i>FAILED</i> to upload files. 😞😞"
                 await user_message.reply_text(
