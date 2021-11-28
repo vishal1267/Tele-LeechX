54497ea92115b88b3363548c64575c20eb59c3d5
vishal1267 <95161786+vishal1267@users.noreply.github.com>
2021-11-28 15:00:59 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -300,7 +300,7 @@ async def rename_tg_file(client, message):
                     f"<a href='tg://user?id={usr_id}'><i>🗃 Your Uploaded Files !!</i></a>\n\n"
                 )
                 message_to_send = mention_req_user + message_to_send
-                message_to_send = message_to_send + "\n\n" + "#Uploads\n\n<b>💥 <i>Powered By : @JNS_BOTS ❤️</i> </b>"
+                message_to_send = message_to_send + "\n\n" + "#Uploads\n\n<b>💥 <i>Powered By : @imTheVishal ❤️</i> </b>"
             else:
                 message_to_send = "<i>FAILED</i> to upload files. 😞😞"
             await message.reply_text(
